f0078255bbeae8df443ce8788cf65d382323915f
MaximusB93 <maksim.budnikov.github@gmail.com>
2022-06-16 21:18:10 +0300

    Сумма четных и нечетных чисел

--- TZ3/Program.cs
+++ TZ3/Program.cs
@@ -8,15 +8,27 @@ namespace TZ3
     {
         static void Main(string[] args)
         {
+            //Сумма четных и нечетных чисел
+            int[] MyArray = { 200, 5, 12, 1, 5, 5, 25 };
 
-            int[] MyArray = { 20, 5, 7, 1, 3, 11, 82, 64, 4, 16 };
+            int SumEvenNumbers = 0;
+            int SumOddNumbers = 0;
 
             for (int i = 0; i < MyArray.Length; i++)
             {
-
+                if (MyArray[i] % 2 == 0)
+                {
+                    SumEvenNumbers += MyArray[i];
+                }
+                else
+                {
+                    SumOddNumbers += MyArray[i];
+                }
             }
 
-            
+            Console.WriteLine("Сумма четных чисел - " + SumEvenNumbers);
+            Console.WriteLine("Сумма нечетных чисел - " + SumOddNumbers);
+
 
         }
     }
