22b05ceaec713051a232817f81935ac836f4e762
MaximusB93 <maksim.budnikov.github@gmail.com>
2022-06-16 20:16:03 +0300

    Добавьте файлы проекта.

---
+++ TZ3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TZ3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();  
+            
+            int [,,] MyArray = new int [4, 3, 5];
+
+            for (int i = 0; i < MyArray.GetLength(0); i++)
+            {
+                for (int y = 0; y < MyArray.GetLength(1); y++)
+                {
+                    for (int r = 0; r < MyArray.GetLength(2); r++)
+                    {
+
+                    }
+
+
+                }
+
+
+            }
+            
+
+        }
+    }
+}
---
+++ TZ3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("TZ3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TZ3")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d5cacda5-55ea-416a-9c1b-f07d0398b8d8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
