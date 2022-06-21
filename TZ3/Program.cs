32601e8b79fa743d2f91ae30552668de728b47e4
MaximusB93 <maksim.budnikov.github@gmail.com>
2022-06-21 14:01:40 +0300

    1

--- TZ3/Program.cs
+++ TZ3/Program.cs
@@ -10,11 +10,8 @@ namespace TZ3
         static void Main(string[] args)
         {
             //Сумма четных и нечетных чисел
-
             Random rand = new Random();
-
             Console.WriteLine("Введите величину массива");
-
             int ValueArray = int.Parse(Console.ReadLine());
             Console.Clear();
             int[] MyArray = new int[ValueArray];
@@ -29,7 +26,6 @@ namespace TZ3
             Console.WriteLine("\n-----------------------\n");
             int SumEvenNumbers = 0;
             int SumOddNumbers = 0;
-
             for (int i = 0; i < MyArray.Length; i++)
             {
                 if (MyArray[i] % 2 == 0)
@@ -41,45 +37,35 @@ namespace TZ3
                     SumOddNumbers += MyArray[i];
                 }
             }
-
             Console.WriteLine("Сумма четных чисел - " + SumEvenNumbers);
             Console.WriteLine("Сумма нечетных чисел - " + SumOddNumbers);
             Console.WriteLine("\n-----------------------\n");
-
 
             //Минимальный и максимальный элемент массива
             //Console.WriteLine(MyArray.Min());
             //Console.WriteLine(MyArray.Max());
 
             //Либо через цикл
-
             int MaxNumber = MyArray[0];
             int MinNumber = MyArray[0];
-
             for (int i = 1; i < MyArray.Length; i++)
             {
                 if (MaxNumber < MyArray[i])
                 {
                     MaxNumber = MyArray[i];
-
                 }
-
                 else if (MinNumber > MyArray[i])
                 {
                     MinNumber = MyArray[i];
                 }
-
             }
             Console.WriteLine("Максимальный элемент массива - " + MaxNumber);
             Console.WriteLine("Минимальный элемент массива - " + MinNumber);
             Console.WriteLine("\n-----------------------\n");
 
             // Нечетный минимальный и четный максимальный
-
             int MaxEvenNumber = MyArray[0];
             int MinOddNumber = MyArray[0];
-
-
             for (int i = 1; i < MyArray.Length; i++)
             {
                 if (MaxEvenNumber < MyArray[i] && MyArray[i] % 2 == 0)
@@ -91,28 +77,35 @@ namespace TZ3
                     MinOddNumber = MyArray[i];
                 }
             }
-
             Console.WriteLine("Четный максимальный элемент - " + MaxEvenNumber);
             Console.WriteLine("Нечетный минимальный элемент - " + MinOddNumber);
+            Console.WriteLine("\n-----------------------\n");
 
-
+            //Перестановка максимального и минимального элементов массива
+            Console.WriteLine("Перестановка максимального и минимального элементов массива");
             int MaxElementArray = MyArray.Max();
             int MinElementArray = MyArray.Min();
-
             for (int i = 0; i < MyArray.Length; i++)
             {
                 if (MyArray[i] == MaxElementArray)
                 {
-                    MaxElementArray = MyArray[i];
+                    Console.Write(MinElementArray + "\t");
                 }
-
                 else if (MyArray[i] == MinElementArray)
                 {
-                    MinElementArray = MyArray[i];
+                    Console.Write(MaxElementArray + "\t");
+                }
+                else
+                {
+                    Console.Write(MyArray[i] + "\t");
                 }
             }
+            Console.WriteLine("\n-----------------------\n");
 
-            Console.WriteLine()
+
+            Array.Sort(MyArray);
+            Array.ForEach(MyArray, Console.Write);
+            Array.ForEach()
 
         }
     }
